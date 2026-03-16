0fa816591edf2fdc07a6a7f039a82a0d34b7d13b
Nate Miller <nsmiller@andrew.cmu.edu>
2026-03-15 20:11:46 -0400

    Initial implementation of motor_driver.

--- pico/leg_controller/motor_driver/motor_driver.c
+++ pico/leg_controller/motor_driver/motor_driver.c
@@ -6,11 +6,11 @@
 
 // Division of sys clock (133MHz (maybe 200MHz overclocked)) for pwm frequency.
 // 8 bit int, 4 bit frac(/16)
-#define PWM_DIV_INT 52
-#define PWM_DIV_FRAC 10
+#define PWM_DIV_INT_MOTOR 52
+#define PWM_DIV_FRAC_MOTOR 10
 
 // Wrap number
-#define PWM_WRAP 100
+#define PWM_WRAP_MOTOR 99
 
 
 
@@ -36,15 +36,14 @@ motor *init_motor(uint pinPlus, uint pinMinus){
     motorDriver->sliceMinus = pwm_gpio_to_slice_num(pinMinus);
     motorDriver->channelMinus = pwm_gpio_to_channel(pinMinus);
 
-    pwm_set_counter(motorDriver->slicePlus, PWM_WRAP);
-    pwm_set_counter(motorDriver->sliceMinus, PWM_WRAP);
-    
     pwm_set_chan_level(motorDriver->slicePlus, motorDriver->channelPlus, 0);
     pwm_set_chan_level(motorDriver->sliceMinus, motorDriver->channelMinus, 0);
 
     pwm_config pwmCfg = pwm_get_default_config();
 
-    pwm_config_set_clkdiv_int_frac4 (&pwmCfg, PWM_DIV_INT, PWM_DIV_FRAC);
+    pwm_config_set_clkdiv_int_frac4 (&pwmCfg, PWM_DIV_INT_MOTOR, PWM_DIV_FRAC_MOTOR);
+
+    pwm_config_set_wrap(&pwmCfg, PWM_WRAP_MOTOR);
 
     pwm_init (motorDriver->slicePlus, &pwmCfg, true);
     pwm_init (motorDriver->sliceMinus, &pwmCfg, true);
@@ -56,10 +55,16 @@ motor *init_motor(uint pinPlus, uint pinMinus){
  * Set power of motor. Takes pointer to motor-driver struct and power (-100, 100) 
  */ 
 void set_motor(motor *driver, int power){
-    int invPower = -1 * power;
-    
     if (driver != NULL) {
-        pwm_set_chan_level(driver->slicePlus, driver->channelPlus, power);
-        pwm_set_chan_level(driver->sliceMinus, driver->channelMinus, invPower);
+        if (power > 0) {
+            pwm_set_chan_level(driver->slicePlus, driver->channelPlus, power);
+            pwm_set_chan_level(driver->sliceMinus, driver->channelMinus, 0);
+        } else if (power < 0) {
+            pwm_set_chan_level(driver->slicePlus, driver->channelPlus, 0);
+            pwm_set_chan_level(driver->sliceMinus, driver->channelMinus, -power);
+        } else {
+            pwm_set_chan_level(driver->slicePlus, driver->channelPlus, 0);
+            pwm_set_chan_level(driver->sliceMinus, driver->channelMinus, 0);
+        }
     }
 }
