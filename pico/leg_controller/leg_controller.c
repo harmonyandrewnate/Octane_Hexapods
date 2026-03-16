b5ed7b275baac21ec1e908ae12157ea944dfe4f4
Nate Miller <nsmiller@andrew.cmu.edu>
2026-03-16 10:49:39 -0400

    Motor test runs from usb command line inputs.

--- pico/leg_controller/leg_controller.c
+++ pico/leg_controller/leg_controller.c
@@ -1,24 +1,94 @@
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 
-const uint LED_PIN = 25;
+#include "stepper_driver.c"
+#include "motor_driver.c"
+#include "encoder.c"
+
+
+
+#define BUFF_LEN 64
+
+#define SWING_A_PLUS 10
+#define SWING_A_MINUS 11
+#define SWING_B_PLUS 12
+#define SWING_B_MINUS 13
+
+#define STEP_POW_LIMIT 10
+
+#define LIFT_PLUS 8
+#define LIFT_MINUS 9
+
+#define ELBOW_PLUS 4
+#define ELBOW_MINUS 5
+
+
+
+typedef struct{
+    stepper *shoulderSwing;
+    motor *shoulderLift;
+    motor *elbow;
+} legModule;
+
+void handle_cmd(char *buff, legModule *leg) {
+    int arg1, arg2, matches;
+    char cmd[BUFF_LEN];
+    matches = sscanf(buff, "%s %i %i", cmd, &arg1, &arg2);
+    if (strcmp(cmd, "stepper") == 0 && matches == 2){
+        set_stepper_speed(leg->shoulderSwing, arg1);
+    } else if (strcmp(cmd, "shoulder") == 0 && matches == 2){
+        set_motor(leg->shoulderLift, arg1);
+    } else if (strcmp(cmd, "elbow") == 0 && matches == 2){
+        set_motor(leg->elbow, arg1);
+        printf("set %d \n", arg1);
+    } 
+}
 
 int main() {
-
-    bi_decl(bi_program_description("This is a test binary."));
-    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
-
     stdio_init_all();
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    legModule leg;
+
+    leg.shoulderSwing = init_stepper(SWING_A_PLUS, SWING_A_MINUS, SWING_B_PLUS, SWING_B_MINUS,
+                                      STEP_POW_LIMIT, 0);
+
+    set_stepper_power(leg.shoulderSwing, 10);    
+
+    leg.shoulderLift = init_motor(LIFT_PLUS, LIFT_MINUS);
+    leg.elbow = init_motor(ELBOW_PLUS, ELBOW_MINUS);
+
+    
+    printf("commands are \"stepper\", \"shoulder\", and \"elbow\" each with 1 arg. \n");
+
+
+    int read_chars = 0;
+    char buff[BUFF_LEN] = {0};
+    char tmp_char = 0;
+
     while (1) {
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 1);
-        puts("Hello World\n");
-        sleep_ms(1000);
+        tmp_char = getchar();
+        if (read_chars < BUFF_LEN) {
+            buff[read_chars] = tmp_char;
+            read_chars++;
+            printf("%c", tmp_char);
+            if (tmp_char == '\r') printf("\n");
+            
+            if (tmp_char == '\n' || tmp_char == '\r') {
+                handle_cmd(buff, &leg);
+                for (int i = 0; i < read_chars; i++) {
+                    buff[i] = 0;
+                }
+                read_chars = 0;
+            }
+        } else {
+            if (tmp_char == '\n' || tmp_char == '\r') {
+                printf("%c", tmp_char);
+                read_chars = 0;
+                tmp_char = '\0';
+            }
+        }
     }
 }
