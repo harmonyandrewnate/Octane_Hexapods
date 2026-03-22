13ee022fe58f2639175d4802d486b7aa8744bf18
Nate Miller <nsmiller@andrew.cmu.edu>
2026-03-22 16:04:21 -0400

    Moved IK.py into repository.

---
+++ le_potato/IK.py
@@ -0,0 +1,44 @@
+import numpy as np
+import math
+
+LINK_1 = 125.58
+LINK_2 = 425
+LINK_3 = 618
+TWIST_1 = np.pi/2
+TWIST_2 = 0
+TWIST_3 = 0
+
+
+def IK(X, Y, Z):
+    theta_1 = math.atan2(Y,X)
+    X = X - (LINK_1 * np.cos(theta_1))
+    Y = Y - (LINK_1 * np.sin(theta_1))
+    hypotenuseSqrd = (X**2) + (Y**2) + (Z**2)
+    if (math.sqrt(hypotenuseSqrd) > LINK_2+LINK_3):
+        return
+    if (math.sqrt(hypotenuseSqrd) < abs(LINK_2-LINK_3)):
+        return
+    theta_2 = math.acos(((LINK_2**2)+(hypotenuseSqrd)-(LINK_3**2)) / (2*LINK_2*math.sqrt(hypotenuseSqrd))) + math.asin(Z/np.sqrt(hypotenuseSqrd))
+    theta_3 = math.acos(((LINK_2**2)+(LINK_3**2)-hypotenuseSqrd) / (2*LINK_2*LINK_3))
+    return [theta_1, theta_2, theta_3]
+
+while (True):
+    print("Input target X position:")
+    X = float(input())
+    print("Input target Y position:")
+    Y = float(input())
+    print("Input target Z position:")
+    Z = float(input())
+
+    angles = IK(X, Y, Z)
+    if angles is None:
+        print("Out of Range")
+    else:
+        angles = np.rad2deg(angles)
+    
+        angles[0] *= -1
+        angles[1] += 0
+        angles[2] += 180
+
+        print("Angles to reach target:")
+        print(angles)
---
+++ le_potato/README.md
@@ -0,0 +1,7 @@
+python needs to be run in a virtual enviroment with the following libraries:
+https://inputs.readthedocs.io/en/latest/
+https://numpy.org/doc/
+
+
+
+
