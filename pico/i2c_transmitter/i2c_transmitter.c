1295a1ab42a6875c18a5248573fd0f159aeb0c72
Nate Miller <nsmiller@andrew.cmu.edu>
2026-04-20 10:53:51 -0400

    Pi Pico serial->I2C repeater

---
+++ pico/i2c_transmitter/i2c_transmitter.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include <hardware/i2c.h>
+
+
+
+
+/*
+ * I2C configuration
+ */
+#define I2C_SDA_PIN 16
+#define I2C_SCL_PIN 17
+#define I2C_BAUDRATE 100000
+#define I2C_SLAVE_BASE_ADDRESS 0x10
+
+// Serial buffer
+#define BUFF_LEN 64
+
+
+
+
+
+void I2C_setup() {
+    i2c_init(i2c0, I2C_BAUDRATE);
+    gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA_PIN);
+    gpio_pull_up(I2C_SCL_PIN);
+}
+
+
+void handle_cmd(char *buff) {
+    int arg1, arg2, matches;
+    char cmd[BUFF_LEN];
+    uint8_t msg;
+    matches = sscanf(buff, "%s %i %i", cmd, &arg1, &arg2);
+    if (strcmp(cmd, "f") == 0 && matches == 1){
+        msg = 'f';
+        i2c_write_blocking(i2c0, 0x00, &msg, 1, false);
+    } else if (strcmp(cmd, "b") == 0 && matches == 1){
+        msg = 'b';
+        i2c_write_blocking(i2c0, 0x00, &msg, 1, false);
+    } else if (strcmp(cmd, "l") == 0 && matches == 1){
+        msg = 'l';
+        i2c_write_blocking(i2c0, 0x00, &msg, 1, false);
+    } else if (strcmp(cmd, "r") == 0 && matches == 1){
+        msg = 'r';
+        i2c_write_blocking(i2c0, 0x00, &msg, 1, false);
+    } else if (strcmp(cmd, "s") == 0 && matches == 1){
+        msg = 's';
+        i2c_write_blocking(i2c0, 0x00, &msg, 1, false);
+    } 
+}
+
+void serial_handler(int read_chars, char *buff, char tmp_char) {
+    tmp_char = getchar();
+    if (read_chars < BUFF_LEN) {
+        buff[read_chars] = tmp_char;
+        read_chars++;
+        printf("%c", tmp_char);
+        if (tmp_char == '\r') printf("\n");
+        
+        if (tmp_char == '\n' || tmp_char == '\r') {
+            handle_cmd(buff);
+            for (int i = 0; i < read_chars; i++) {
+                buff[i] = 0;
+            }
+            read_chars = 0;
+        }
+    } else {
+        if (tmp_char == '\n' || tmp_char == '\r') {
+            printf("%c", tmp_char);
+            read_chars = 0;
+            tmp_char = '\0';
+        }
+    }
+}
+
+
+void main(){
+    I2C_setup();    
+
+    int read_chars = 0;
+    char buff[BUFF_LEN] = {0};
+    char tmp_char = 0;
+
+
+    serial_handler(read_chars, buff, tmp_char);
+}
