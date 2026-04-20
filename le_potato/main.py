4b17ac10934a22be4068934f014974255f71b348
Nate Miller <nsmiller@andrew.cmu.edu>
2026-04-20 10:55:10 -0400

    Updated serial write format.

--- le_potato/main.py
+++ le_potato/main.py
@@ -7,22 +7,24 @@ import serial
 
 def main():
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-
+    
+    ser.write(b'\n\n\n\n\n\n')
+    
     while True:
         events = get_gamepad()
         for event in events:
             if event.ev_type == 'button':
                 if event.state == 0:
-                    ser.write(b's')
+                    ser.write(b's\n')
                 else:
                     if event.code == 'BTN_WEST':
-                        ser.write(b'f')
+                        ser.write(b'f\n')
                     elif event.code == 'BTN_SOUTH':
-                        ser.write(b'b')
+                        ser.write(b'b\n')
                     elif event.code == 'BTN_NORTH':
-                        ser.write(b'l')
+                        ser.write(b'l\n')
                     elif event.code == 'BTN_EAST':
-                        ser.write(b'r')
+                        ser.write(b'r\n')
 
 
 
